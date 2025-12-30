79ad70dfa2c530c70d9c25c2399058b6bffe1545
choijhn <jhchoi4912@gmail.com>
2025-12-30 15:40:01 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/kangaroo/sparring/SparringApplication.java
@@ -0,0 +1,13 @@
+package com.kangaroo.sparring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparringApplication.class, args);
+	}
+
+}
